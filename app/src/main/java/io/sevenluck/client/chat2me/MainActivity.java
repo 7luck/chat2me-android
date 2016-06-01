eca4abcd1344a1ed28b9919c26212114aed5025b
lukas <loki@linux-e0xq.Speedport_W_921V_1_39_000>
2016-06-01 20:55:32 +0200

    - test commit

--- app/src/main/java/io/sevenluck/client/chat2me/MainActivity.java
+++ app/src/main/java/io/sevenluck/client/chat2me/MainActivity.java
@@ -3,6 +3,10 @@ package io.sevenluck.client.chat2me;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+
+/**
+ * test commit
+ */
 public class MainActivity extends AppCompatActivity {
 
     @Override
