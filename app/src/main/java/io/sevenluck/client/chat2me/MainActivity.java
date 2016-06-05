fe578f8b7feca0ff743a34963ede241e5e64cf8f
lukas <loki@linux-e0xq.Speedport_W_921V_1_39_000>
2016-06-05 14:42:10 +0200

    loading

--- app/src/main/java/io/sevenluck/client/chat2me/MainActivity.java
+++ app/src/main/java/io/sevenluck/client/chat2me/MainActivity.java
@@ -1,5 +1,6 @@
 package io.sevenluck.client.chat2me;
 
+import android.app.ProgressDialog;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -19,10 +20,11 @@ import io.sevenluck.client.chat2me.tasks.callbacks.HttpRequestCallback;
  */
 public class MainActivity extends AppCompatActivity {
 
-    private EditText    nicknameTb;
-    private EditText    passwordTb;
-    private Button      loginBtn;
-    private Button      registerBtn;
+    private EditText        nicknameTb;
+    private EditText        passwordTb;
+    private Button          loginBtn;
+    private Button          registerBtn;
+    private ProgressDialog  dialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,7 +35,7 @@ public class MainActivity extends AppCompatActivity {
         registerBtn = (Button) findViewById(R.id.registerBtn);
         nicknameTb  = (EditText) findViewById(R.id.nicknameText);
         passwordTb  = (EditText) findViewById(R.id.passwordEdit);
-
+        dialog      = new ProgressDialog(this);
 
 
         registerBtn.setOnClickListener(new View.OnClickListener() {
@@ -41,6 +43,9 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 RegisterAsyncTask regTask = new RegisterAsyncTask(MainActivity.this, new RegistrationCallback());
                 registerBtn.setEnabled(false);
+
+                showDialog();
+
                 Member member = new Member();
                 member.setNickname(nicknameTb.getText().toString());
                 member.setPassword(passwordTb.getText().toString());
@@ -50,12 +55,18 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    private void showDialog() {
+        dialog.setTitle("Loading");
+        dialog.setMessage("Waiting while Loading...");
+        dialog.show();
+    }
+
     public class RegistrationCallback extends HttpRequestCallback<HttpResult<Member>> {
 
         @Override
         public void onFishedRequest(HttpResult<Member> result) {
             registerBtn.setEnabled(true);
-
+            dialog.dismiss();
             if (null == result) {
                 return;
             }
