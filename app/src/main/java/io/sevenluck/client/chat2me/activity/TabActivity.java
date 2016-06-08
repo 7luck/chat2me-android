a9c1483b3130617be2dddbf7cf585e8fd0c18968
lukas <loki@linux-e0xq.Speedport_W_921V_1_39_000>
2016-06-08 22:15:58 +0200

    pretty color for appname

--- app/src/main/java/io/sevenluck/client/chat2me/activity/TabActivity.java
+++ app/src/main/java/io/sevenluck/client/chat2me/activity/TabActivity.java
@@ -1,13 +1,19 @@
 package io.sevenluck.client.chat2me.activity;
 
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.content.ContextCompat;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.support.design.widget.TabLayout;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -29,6 +35,10 @@ public class TabActivity extends AppCompatActivity {
         setContentView(R.layout.activity_tab);
 
         toolbar = (Toolbar) findViewById(R.id.toolbar);
+
+        toolbar.setTitle(getText(R.string.app_name));
+
+        toolbar.setTitleTextColor(getResources().getColor(R.color.colorAccent));
         setSupportActionBar(toolbar);
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
@@ -42,6 +52,26 @@ public class TabActivity extends AppCompatActivity {
         tabLayout.getTabAt(0).setIcon(R.drawable.ic_chat_black_24dp);
         tabLayout.getTabAt(1).setIcon(R.drawable.ic_people_black_24dp);
         tabLayout.getTabAt(2).setIcon(R.drawable.ic_settings_black_24dp);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_toolbar, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            // action with ID action_refresh was selected
+            case R.id.menu_item_logout:
+                Toast.makeText(this, "Logout me", Toast.LENGTH_SHORT).show();
+                break;
+            default:
+                break;
+        }
+        return true;
     }
 
     private void setupViewPager(ViewPager viewPager) {
